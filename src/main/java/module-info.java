e8d61a3e7f0c12cad6b75350e117434c949f82cd
brunoKayser <bruno.kaysers@gmail.com>
2025-05-12 19:46:54 -0300

    atualizacao-java-21 Removendo module-info

--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module GenericFixture {
-    requires org.apache.commons.lang3;
-    requires rgxgen;
-    requires lombok;
-    requires jakarta.validation;
-    exports generic.fixture;
-}
--- src/test/java/domain/DomainMapError.java
+++ src/test/java/domain/DomainMapError.java
@@ -20,5 +20,4 @@ public class DomainMapError {
 
     private TreeMap<ComplexType, Integer>[] treeMapComplexTypeInteger;
 
-
 }
